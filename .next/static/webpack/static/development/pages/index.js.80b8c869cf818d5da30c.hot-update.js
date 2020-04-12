b6b7d9f8bde24febebd31b470454f6cdcf5f72ef
iyamus <iyamus@gmail.com>
2020-04-11 19:55:24 -0600

    initial version_test

---
+++ .next/static/webpack/static/development/pages/index.js.80b8c869cf818d5da30c.hot-update.js
@@ -0,0 +1,19 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return test;
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.80b8c869cf818d5da30c.hot-update.js.map
